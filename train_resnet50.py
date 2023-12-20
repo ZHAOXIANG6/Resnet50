6e8ca7dbb800d4b7f0a68869f62751ea86f6f43c
XIANG <145321381+ZHAOXIANG6@users.noreply.github.com>
2023-12-20 20:15:38 +0900

    Add files via upload

---
+++ train_resnet50.py
@@ -0,0 +1,114 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+from torchvision.datasets import CIFAR10
+from torchvision import  transforms
+from torchvision.transforms import ToTensor, Normalize
+from torchvision.models import resnet50
+
+plt.rcParams['font.size'] = 15
+# 设置设备
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("-------------------------------------")
+print("running on {}".format(device))
+
+# 数据预处理和加载
+transform = transforms.Compose([
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomCrop(32, padding=4),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                         std=[0.229, 0.224, 0.225])
+])
+
+train_dataset = CIFAR10(root='./data', train=True, download=True, transform=transform)
+train_loader = DataLoader(train_dataset, batch_size=128, shuffle=True)
+
+test_dataset = CIFAR10(root='./data', train=False, download=True, transform=transform)
+test_loader = DataLoader(test_dataset, batch_size=128, shuffle=False)
+
+# 创建ResNet-50模型
+model = resnet50(pretrained=False)
+num_features = model.fc.in_features
+model.fc = nn.Linear(num_features, 10)
+model = model.to(device)
+
+# 定义损失函数和优化器
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+# 初始化损失和准确率的列表
+train_losses = []
+test_losses = []
+test_accuracies = []
+
+# 训练模型
+num_epochs = 120
+for epoch in range(num_epochs):
+    model.train()
+    running_loss = 0.0
+    for i, (inputs, labels) in enumerate(train_loader):
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+
+        optimizer.zero_grad()
+
+        # 前向传播和反向传播
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+    # 在测试集上计算损失和准确率
+    model.eval()
+    test_loss = 0.0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for inputs, labels in test_loader:
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+
+            test_loss += loss.item()
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    # 计算平均损失和准确率
+    avg_train_loss = running_loss / len(train_loader)
+    avg_test_loss = test_loss / len(test_loader)
+    test_accuracy = 100 * correct / total
+
+    # 将指标添加到列表中
+    train_losses.append(avg_train_loss)
+    test_losses.append(avg_test_loss)
+    test_accuracies.append(test_accuracy)
+
+    print('Epoch [{}/{}], Train Loss: {:.4f}, Test Loss: {:.4f}, Test Accuracy: {:.2f}%'
+          .format(epoch+1, num_epochs, avg_train_loss, avg_test_loss, test_accuracy))
+torch.save(model.state_dict(),
+               './checkpoints/resnet50.pth')
+
+# 绘制损失和准确率的图表
+plt.figure(figsize=(10, 5))
+plt.subplot(1, 2, 1)
+plt.plot(range(1, num_epochs+1), train_losses, label='Train Loss')
+plt.plot(range(1, num_epochs+1), test_losses, label='Test Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
+
+plt.subplot(1, 2, 2)
+plt.plot(range(1, num_epochs+1), test_accuracies, label='Test Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend()
+
+plt.tight_layout()
+plt.show()
